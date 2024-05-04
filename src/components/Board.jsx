0f7c38d06876bb7c3bb882a9d025649fb50d8a4d
jonathan296 <55729464+jonathan296@users.noreply.github.com>
2024-05-04 15:42:19 -0700

    finished xo hover states and styling

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Square from "./Square";
+import { useNavigate } from "react-router-dom";
 
 export default function Board() {
 const [currentPlayer, setCurrentPlayer] = useState('X')
-
+const navigate = useNavigate();
 
 const togglePlayer = () => {
         setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
@@ -16,9 +17,9 @@ const togglePlayer = () => {
     <div className="flex flex-col justify-center items-center h-screen ">
       <div className="w-[460px] h-[623px]">
         <div className="grid grid-cols-3 w-full items-center mb-[19px] ">
-          <img className="justify-self-start" src="./xoLogo.svg"></img>
-          <div className="justify-self-center bg-[#1F3641] shadow-[0px_-4px_0px_#10212A_inset] w-[140px] h-[52px] rounded-[10px]  flex ">
-            {currentPlayer==='X'?(<img src="./xTurnIcon.svg"></img>):(<img src="./oTurnIcon.svg"></img>)}<p>turn</p>
+          <img onClick={()=>{navigate('/')}} title="Home" className="cursor-pointer justify-self-start" src="./xoLogo.svg"></img>
+          <div className="justify-self-center bg-[#1F3641] shadow-[0px_-4px_0px_#10212A_inset] w-[140px] h-[52px] rounded-[10px]  flex justify-center items-center gap-[13px] pb-[4px] ">
+            {currentPlayer==='X'?(<img className="w-[20px] h-[20px]" src="./xTurnIcon.svg"></img>):(<img className="w-[20px] h-[20px] " src="./oTurnIcon.svg"></img>)}<p className="text-[#A8BFC9] font-outfit text-[16px] font-bold tracking-[1px]">TURN</p>
           </div>
           <div className="justify-self-end bg-[#A8BFC9] shadow-[0px_-4px_0px_#6B8997_inset] w-[52px] h-[52px] rounded-[10px]"></div>
         </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,20 +2,41 @@ import { useState } from "react"
 
 export default function Square({player, togglePlayer}){
     const [content, setContent] = useState(null)
-    
+    const [hovered, setHovered] = useState(false)
+
+
     const handleClick = () => {
         if (content === null) { // Only set content if it's not already set
             setContent(player);
+            setHovered(false)
             togglePlayer();
         }
     }
 
+    const handleMouseEnter = () => {
+        if(content === null){
+            setHovered(true)
+        }
+        
+        
+    }
+    const handleMouseLeave = () => {
+       
+        setHovered(false)
+        
+    }
+    const displayContent = () => {
+        
+    }
+
+
     return(
-        <button onClick={handleClick} onMouseEnter={()=>{}} onMouseLeave={()=>{}} className="bg-[#1F3641] w-[140px] h-[140px] rounded-[15px]  shadow-[0px_-8px_0px_#10212A_inset]" >
-            <div className="text-2xl">
-               {content}
-            </div>
+        <button onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="bg-[#1F3641] w-[140px] h-[140px] rounded-[15px]  shadow-[0px_-8px_0px_#10212A_inset]" >
             
+            {content === "X" ? <img className="m-auto" src="./xSelectedIcon.svg"></img>:<img></img>}
+            {content === "O" ? <img className="m-auto" src="./oSelectedIcon.svg"></img>:<img></img>}
+            {player === "X"&& hovered?(<img className="m-auto" src="./xHoverIcon.svg"></img>):(<img></img>)}
+            {player === "O"&& hovered?(<img className="m-auto" src="./oHoverIcon.svg"></img>):(<img></img>)}
         </button>
     )
 }
