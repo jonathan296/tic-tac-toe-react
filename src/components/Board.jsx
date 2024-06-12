8c22f04bf4bbf8f3f651623eac6b5c2f984016ce
jonathan296 <55729464+jonathan296@users.noreply.github.com>
2024-06-11 18:55:20 -0700

    Implemented score counter

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Square from "./Square";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Board() {
   // Hooks
@@ -8,11 +9,24 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [winner, setWinner] = useState(null);
   const [winningLine, setWinningLine] = useState([]);
+  const [gameReset, setgameReset] = useState(false)
+  const [xScore, setXScore] = useState(0)
+  const [tieScore, setTieScore] = useState(0)
+  const [oScore, setOScore] = useState(0)
+
+
+
   const navigate = useNavigate();
 
   // Functions
   const togglePlayer = () => {
     setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  };
+
+  useEffect(()=>{handleScores(winner)},[winner]);
+
+  const handleScores = (winner) => {
+    winner==="X" && setXScore(xScore + 1)
   };
 
  
@@ -63,14 +77,38 @@ export default function Board() {
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="w-[460px] h-[623px]">
         <div className="grid grid-cols-3 w-full items-center mb-[19px]">
+          
           <img onClick={() => { navigate('/') }} title="Home" className="cursor-pointer justify-self-start" src="./xoLogo.svg" alt="Home"></img>
+          
+          
           <div className="justify-self-center bg-[#1F3641] shadow-[0px_-4px_0px_#10212A_inset] w-[140px] h-[52px] rounded-[10px] flex justify-center items-center gap-[13px] pb-[4px]">
             {currentPlayer === 'X' ? (<img className="w-[20px] h-[20px]" src="./xTurnIcon.svg" alt="X's turn"></img>) : (<img className="w-[20px] h-[20px]" src="./oTurnIcon.svg" alt="O's turn"></img>)}
             <p className="text-[#A8BFC9] font-outfit text-[16px] font-bold tracking-[1px]">TURN</p>
           </div>
-          <div onClick={() => { navigate('/') }} className="flex justify-center flex-col items-center cursor-pointer justify-self-end bg-[#A8BFC9] shadow-[0px_-4px_0px_#6B8997_inset] w-[52px] h-[52px] rounded-[10px]">
+          <div onClick={() => { setgameReset(true) }} className="flex justify-center flex-col items-center cursor-pointer justify-self-end bg-[#A8BFC9] shadow-[0px_-4px_0px_#6B8997_inset] w-[52px] h-[52px] rounded-[10px]">
             <img src="./redo.svg" className="w-[20px] h-[20px] "></img>
           </div>
+          {gameReset && (
+          <div>
+
+
+            <div className="fixed  inset-0 bg-black opacity-50 w-full h-full"></div>
+            <div className="fixed inset-0 flex  items-center justify-center">
+            <div className=" h-[266px] w-full bg-[#1F3641] flex flex-col items-center justify-center ">
+              <div className={`flex text-[40px] tracking-[2.5px] mb-[21px] gap-[24px] font-outfit font-bold text-[#A8BFC9]`}>
+                <h1>RESTART GAME?</h1>
+              </div>
+              <div className="font-outfit flex gap-[16px]">
+                <button onClick={() => { setgameReset(false)}} title="Home" className=" h-[52px] w-[139px] bg-[#A8BFC9] hover:bg-[#DBE8ED] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#6B8997_inset] ">NO, CANCEL</button>
+                <button onClick={() => { setWinner(null), setSquares(Array(9).fill(null)), setWinningLine([]),setgameReset(false)}} className=" h-[52px] w-[151px] bg-[#F2B137] hover:bg-[#FFC860] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#CC8B13_inset] ">YES, RESTART</button>
+              </div>
+            </div>
+            
+          </div>
+          </div>
+          
+          
+        )}
         </div>
         <div className="grid grid-cols-3 w-fit m-auto gap-[20px]">
           {squares.map((square, index) => 
@@ -84,11 +122,20 @@ export default function Board() {
             />
           )}
         </div>
-        {/*<div className="flex gap-[20px] mt-[19px]">
-          <div className="bg-[#31C3BD] w-[140px] h-[72px] rounded-[15px]"></div>
-          <div className="bg-[#A8BFC9] w-[140px] h-[72px] rounded-[15px]"></div>
-          <div className="bg-[#F2B137] w-[140px] h-[72px] rounded-[15px]"></div>
-        </div>*/}
+        <div className="flex gap-[20px] mt-[19px]">
+          <div className="bg-[#31C3BD] w-[140px] h-[72px] rounded-[15px]">
+            <h2>X(YOU)</h2>
+            {xScore}
+          </div>
+          <div className="bg-[#A8BFC9] w-[140px] h-[72px] rounded-[15px]">
+            <h2>TIE</h2>
+            {tieScore}
+          </div>
+          <div className="bg-[#F2B137] w-[140px] h-[72px] rounded-[15px]">
+            <h2>O(CPU)</h2>
+            {tieScore}
+          </div>
+        </div>
         {winner && (
           <div>
 
@@ -96,12 +143,12 @@ export default function Board() {
             <div className="fixed  inset-0 bg-black opacity-50 w-full h-full"></div>
             <div className="fixed inset-0 flex  items-center justify-center">
             <div className=" h-[266px] w-full bg-[#1F3641] flex flex-col items-center justify-center ">
-              <div className={`flex text-[40px] font-outfit font-bold  ${winner==="X"?"text-[#31C3BD]":"text-white"}`}>
+              <div className={`flex text-[40px] tracking-[2.5px] mb-[21px] gap-[24px] font-outfit font-bold   ${winner==="X"?"text-[#31C3BD]":"text-[#F2B137]"}`}>
                 <img className="w-[64px] h-[64px]" src={winner=== "X"?"xSelectedIcon.svg":"oSelectedIcon.svg"}/> <h1>TAKES THE ROUND!</h1>
               </div>
               <div className="font-outfit flex gap-[16px]">
                 <button onClick={() => { navigate('/') }} title="Home" className=" h-[52px] w-[76px] bg-[#A8BFC9] hover:bg-[#DBE8ED] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#6B8997_inset]  ">QUIT</button>
-                <button onClick={() => { setWinner(null) }} className=" h-[52px] w-[146px] bg-[#F2B137] hover:bg-[#FFC860] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#CC8B13_inset]  ">NEXT ROUND</button>
+                <button onClick={() => { setWinner(null), setSquares(Array(9).fill(null)), setWinningLine([])}} className=" h-[52px] w-[146px] bg-[#F2B137] hover:bg-[#FFC860] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#CC8B13_inset]  ">NEXT ROUND</button>
               </div>
             </div>
             
