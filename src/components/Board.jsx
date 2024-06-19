582eb348ba3c8ad9c08369e782a27770839e5e03
jonathan296 <55729464+jonathan296@users.noreply.github.com>
2024-06-19 14:28:27 -0700

    Completley fineshed score counters

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -11,6 +11,7 @@ export default function Board() {
   const [winningLine, setWinningLine] = useState([]);
   const [gameReset, setgameReset] = useState(false)
   const [xScore, setXScore] = useState(0)
+  const [isTie, setIsTie] = useState(false)
   const [tieScore, setTieScore] = useState(0)
   const [oScore, setOScore] = useState(0)
 
@@ -23,10 +24,21 @@ export default function Board() {
     setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
   };
 
-  useEffect(()=>{handleScores(winner)},[winner]);
+  useEffect(()=>{
+    handleScores(winner)
+  },[winner, squares]);
 
   const handleScores = (winner) => {
-    winner==="X" && setXScore(xScore + 1)
+    const tieCheck = squares.every(square => square !== null)
+    // checks if every square is used 
+    winner==="X" && setXScore(xScore + 1);
+    winner==="O" && setOScore(oScore + 1);
+
+    if(winner === null && tieCheck === true){
+      setTieScore(tieScore + 1);
+      setIsTie(true)
+    }
+    
   };
 
  
@@ -123,23 +135,23 @@ export default function Board() {
           )}
         </div>
         <div className="flex gap-[20px] mt-[19px]">
-          <div className="bg-[#31C3BD] w-[140px] h-[72px] rounded-[15px]">
-            <h2>X(YOU)</h2>
-            {xScore}
+          <div className="flex flex-col justify-center items-center font-outfit text-[#1A2A33] bg-[#31C3BD] w-[140px] h-[72px] rounded-[15px]">
+            <h2 className="tracking-[.88px] font-medium text-[14px] leading-[18px]">X (YOU)</h2>
+            <h1 className="tracking-[1.5px] font-bold text-[24px] leading-[30px]">{xScore}</h1>
           </div>
-          <div className="bg-[#A8BFC9] w-[140px] h-[72px] rounded-[15px]">
-            <h2>TIE</h2>
-            {tieScore}
+          
+          <div className="flex flex-col justify-center items-center font-outfit text-[#1A2A33] bg-[#A8BFC9] w-[140px] h-[72px] rounded-[15px]">
+            <h2 className="tracking-[.88px] font-medium text-[14px] leading-[18px]">TIES</h2>
+            <h1 className="tracking-[1.5px] font-bold text-[24px] leading-[30px]">{tieScore}</h1>
           </div>
-          <div className="bg-[#F2B137] w-[140px] h-[72px] rounded-[15px]">
-            <h2>O(CPU)</h2>
-            {tieScore}
+
+          <div className="flex flex-col justify-center items-center font-outfit text-[#1A2A33] bg-[#F2B137] w-[140px] h-[72px] rounded-[15px]">
+            <h2 className="tracking-[.88px] font-medium text-[14px] leading-[18px]">O (CPU)</h2>
+            <h1 className="tracking-[1.5px] font-bold text-[24px] leading-[30px]">{oScore}</h1>
           </div>
         </div>
         {winner && (
           <div>
-
-
             <div className="fixed  inset-0 bg-black opacity-50 w-full h-full"></div>
             <div className="fixed inset-0 flex  items-center justify-center">
             <div className=" h-[266px] w-full bg-[#1F3641] flex flex-col items-center justify-center ">
@@ -149,6 +161,25 @@ export default function Board() {
               <div className="font-outfit flex gap-[16px]">
                 <button onClick={() => { navigate('/') }} title="Home" className=" h-[52px] w-[76px] bg-[#A8BFC9] hover:bg-[#DBE8ED] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#6B8997_inset]  ">QUIT</button>
                 <button onClick={() => { setWinner(null), setSquares(Array(9).fill(null)), setWinningLine([])}} className=" h-[52px] w-[146px] bg-[#F2B137] hover:bg-[#FFC860] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#CC8B13_inset]  ">NEXT ROUND</button>
+              </div>
+            </div>
+            
+          </div>
+          </div>
+          
+          
+        )}
+        {isTie && (
+          <div>
+            <div className="fixed  inset-0 bg-black opacity-50 w-full h-full"></div>
+            <div className="fixed inset-0 flex  items-center justify-center">
+            <div className=" h-[266px] w-full bg-[#1F3641] flex flex-col items-center justify-center ">
+              <div className={`flex text-[40px] tracking-[2.5px] mb-[21px] gap-[24px] font-outfit font-bold text-[#A8BFC9]`}>
+                <h1>ROUND TIED</h1>
+              </div>
+              <div className="font-outfit flex gap-[16px]">
+                <button onClick={() => { navigate('/') }} title="Home" className=" h-[52px] w-[76px] bg-[#A8BFC9] hover:bg-[#DBE8ED] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#6B8997_inset]  ">QUIT</button>
+                <button onClick={() => { setIsTie(false), setWinner(null), setSquares(Array(9).fill(null)), setWinningLine([])}} className=" h-[52px] w-[146px] bg-[#F2B137] hover:bg-[#FFC860] text-[#1A2A33] text-[16px] rounded-[10px] font-outfit font-bold shadow-[0px_-4px_0px_#CC8B13_inset]  ">NEXT ROUND</button>
               </div>
             </div>
             
